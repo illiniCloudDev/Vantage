49bd692e364cc748c941005855b0155473931fe7
Charlie Caneda <canedacharlie@gmail.com>
2026-05-08 18:11:32 -0500

    Made adjustments to app.jsx to handle login/logout logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import {
     Route,
     createBrowserRouter, 
     createRoutesFromElements,
     RouterProvider
 } from 'react-router-dom';
+import axios from 'axios';
 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
@@ -16,21 +17,49 @@ import NewsPage from './pages/NewsPage';
 
 const App =  () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-    const Router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<MainLayout isLoggedIn = {isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}>
-            <Route index element={<HomePage />} />
-            <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn}/>} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/news" element={<NewsPage />} />
-        </Route>
-    )
-);
+
+    //check authentication 
+    useEffect(() =>{
+        const checkAuthStatus = async () => {
+            try {
+                const res = await axios.get('/api/auth/me');
+                if(res.data.success){
+                    setIsLoggedIn(true);
+                }
+            } catch (err) {
+                setIsLoggedIn(false);
+                console.log('No active session found.');
+            }finally{
+                setLoading(false)
+            }
+        };
+        checkAuthStatus();
+    }, []);
+
+    const router = useMemo(() => createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<MainLayout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}>
+                <Route index element={<HomePage />} />
+                <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn}/>} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/news" element={<NewsPage />} />
+            </Route>
+        )
+    ), [isLoggedIn]); // Only recreate if login status changes
+
+    if(loading){
+        return(
+            <div className="min-h-screen bg-[#020617] flex items-center justify-center"> 
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-[#38bdf8]"> </div>
+            </div>
+        );
+    }
 
     return (
-        <RouterProvider router={Router} />
+        <RouterProvider router={router} />
     )
 }
 
