5376137bee845230b2c84fecc6f5dd79add19d2d
Charlie Caneda <canedacharlie@gmail.com>
2026-05-08 19:17:00 -0500

    Created a package.json for deployment on Render. Backend server was adjusted to serve our frontend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const Transaction = require('./models/Transaction');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth');
 const transactions = require('./routes/transactions');
+const path = require('path');
 
 
 dotenv.config({path: './.env'});
@@ -15,6 +16,7 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const frontendPath = path.join(__dirname, '..', 'frontend', 'dist')
 
 // Middleware
 app.use(cors());
@@ -22,13 +24,16 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes)
 app.use('/api/transactions', transactions)
+app.use(express.static(frontendPath))
 
 //Test Route
+// app.get('/', (req, res) => {
+//     res.send('Vantage is online!');
+// });
 
-app.get('/', (req, res) => {
-    res.send('Vantage is online!');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
-
 
 // Start the server
 app.listen(PORT, () => {
