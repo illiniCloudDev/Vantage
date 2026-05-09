e33550ceec611bce20beed21252c9a5f22e7a3ef
Charlie Caneda <canedacharlie@gmail.com>
2026-05-08 20:00:25 -0500

    config adjustment

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url';
 import path from 'path'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -22,5 +24,10 @@ export default defineConfig({
         secure: false,
       },
     },
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
