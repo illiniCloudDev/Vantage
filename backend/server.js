f956a3e543e7712c8c6e32c6ebe7415ef861930a
Charlie Caneda <canedacharlie@gmail.com>
2026-05-10 17:57:30 -0500

    made a an api.js file to handle current web address.

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
-  'templateholder'
+  'https://vantage-frontend-rbkk.onrender.com' //live URL of Frontend
 ];
 
 // Middleware
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import api from '../services/api'
 
 const LoginPage = ({setIsLoggedIn}) => {
   const navigate = useNavigate(); //initialize the useNavigate hook
