b83e869b8d2aa3160414c9cdf99ff870840f4ad9
Charlie Caneda <canedacharlie@gmail.com>
2026-05-11 17:34:17 -0500

    set secure to true in options

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -13,7 +13,7 @@ const sendTokenResponse = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production' || true, // Only use HTTPS in production
+    secure: true, // Only use HTTPS in production
     sameSite: 'none',
   };
 
